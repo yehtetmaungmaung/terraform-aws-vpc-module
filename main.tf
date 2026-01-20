a2407468ec8dad4d82cc5cb52d71fa17a49ea436
Ye Htet Maung Maung <yehtetmgmgse@gmail.com>
2026-01-20 09:58:35 +0630

    bump

--- main.tf
+++ main.tf
@@ -7,12 +7,14 @@ locals {
 
 }
 
-#checkov:skip=CKV2_AWS_12:Ensure the default security group of every VPC restricts all traffic
-#checkov:skip=CKV2_AWS_11:VPC Flow Logs are meant to be enabled by terraform-aws-vpc-flow-logs-s3-bucket
+
 #trivy:ignore:AVD-AWS-0178:VPC Flow Logs are meant to be enabled by terraform-aws-vpc-flow-logs-s3-bucket
 resource "aws_vpc" "this" {
 
   cidr_block = var.cidr
+
+  #checkov:skip=CKV2_AWS_12:Ensure the default security group of every VPC restricts all traffic
+  #checkov:skip=CKV2_AWS_11:VPC Flow Logs are meant to be enabled by terraform-aws-vpc-flow-logs-s3-bucket
 
   enable_dns_hostnames = var.enable_dns_hostnames
   enable_dns_support   = var.enable_dns_support
@@ -306,11 +308,12 @@ locals {
 }
 
 
-#checkov:skip=CKV2_AWS_19:Ensure that all EIP addresses allocated to a VPC are attached to EC2 instances
+
 resource "aws_eip" "nat" {
   count = var.enable_nat_gateway && !var.reuse_nat_ips ? local.nat_gateway_count : 0
 
   domain = "vpc"
+  #checkov:skip=CKV2_AWS_19:Ensure that all EIP addresses allocated to a VPC are attached to EC2 instances
 
   tags = merge(
     {
